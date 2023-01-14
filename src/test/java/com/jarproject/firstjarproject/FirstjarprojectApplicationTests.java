05c66360af66f59c63c51e41d6691751d292a632
Rahul Choudhary <rchoudhary@evolution.com>
2023-01-14 22:02:19 +0530

    first commit

---
+++ src/test/java/com/jarproject/firstjarproject/FirstjarprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarproject.firstjarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjarprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
